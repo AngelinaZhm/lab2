d5d10844c2397cb7d1cb6f3f5e819411ae3d8bc3
AngelinaZhm <162491712+AngelinaZhm@users.noreply.github.com>
2026-02-06 12:03:02 +0200

    Add files via upload

---
+++ src/main/java/com/example/lab2/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.lab2.controller;
+
+
+import com.example.lab2.model.User;
+import com.example.lab2.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
